3638733ccd41e6d578cc855f6241fc1418d1c73f
Troy Prujean <9930285@student.toiohomai.ac.nz>
2018-09-14 11:35:44 +1200

    fixed navbar links

---
+++ javascript/javascript.js
@@ -0,0 +1,17 @@
+let hover = Array.from(document.querySelectorAll('li'));
+
+console.log(hover)
+
+hover.map(m => {
+    console.log(m)
+    m.addEventListener('mouseover', function (){
+    //this.firstChild.classList.add('hovertext')
+    this.children[0].classList.add('hovertext')
+    })
+    m.addEventListener('mouseout', function () {
+        this.children[0].classList.remove('hovertext')
+    })
+    m.addEventListener('click', function () {
+    window.location.href = `/${this.children[0].getAttribute('href')}`;
+    })
+})
