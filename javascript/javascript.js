60c9dac8d389a25c61792dae1aee5f44f8b90928
troy prujean <9930285@student.toiohomai.ac.nz>
2018-09-16 09:49:25 +1200

    finish assignment part of website

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,4 +1,4 @@
-/* Code to make entire li item clickable not just text */
+/* Code to make entire navbar items clickable not just the text */
 let hover = Array.from(document.querySelectorAll('li'));
 
 console.log(hover)
@@ -17,67 +17,85 @@ hover.map(m => {
 
 /* Form submission validation */
 function Validate() {
+
+    // Prevent the form from submitting before validation
+    event.preventDefault();
+
+    // Assigning the required form fields to variables
     var name = document.forms["myForm"]["name"].value;
     var phone = document.forms["myForm"]["phone"].value;
     var email = document.forms["myForm"]["email"].value;
     var message = document.forms["myForm"]["message"].value;
 
-    // regEx validation for email field
+    // Object array for storing validation states for each field
+    var validated = [{
+        name: false,
+        phone: false,
+        email: false,
+        message: false
+    }];
+
+    // regular expression validation for email field
     var filter = /^([a-zA-Z0-9_.-])+@(([a-zA-Z0-9-])+.)+([a-zA-Z0-9]{2,4})+$/;
 
-    // prevent the form from submitting before validation
-    event.preventDefault();
-
+    // If the name field is blank display an error, otherwise remove the error and set its validated state to "true"
     if (name == "") {
         document.getElementById("name-error").innerHTML = "Please enter your name";
         document.getElementById("required-button").style.color = "red";
         document.getElementById("required-button").innerHTML = "<br>* Required Fields";
         myForm.name.focus();
-        return false;
     }
-    else document.getElementById("name-error").innerHTML = "";
+    else {
+        document.getElementById("name-error").innerHTML = "";
+        validated.name = true;
+    }
 
-
+    // If the phone field is blank display an error, otherwise remove the error and set its validated state to "true"
     if (phone == "") {
         document.getElementById("phone-error").innerHTML = "Please enter your phone number";
         document.getElementById("required-button").style.color = "red";
         document.getElementById("required-button").innerHTML = "<br>* Required Fields";
         myForm.phone.focus();
-        return false;
     }
-    else document.getElementById("phone-error").innerHTML = "";
+    else {
+        document.getElementById("phone-error").innerHTML = "";
+        validated.phone = true;
+    }
 
+    // If the email field is blank or does not match the regular expression format - display an error, otherwise remove the error and set its validated state to "true"
     if (email == "") {
         document.getElementById("email-error").innerHTML = "Please enter your email address";
         document.getElementById("required-button").style.color = "red";
         document.getElementById("required-button").innerHTML = "<br>* Required Fields";
         myForm.email.focus();
-        return false;
     }
-    // further validating email field
-    else if (!filter.test(email)) {
+    else if (!filter.test(email)) { // Checking to see if the email field matches the template of the regular expression variable called filter
         document.getElementById("email-error").innerHTML = "Please enter a valid e-mail address";
         document.getElementById("required-button").style.color = "red";
         document.getElementById("required-button").innerHTML = "<br>* Required Fields";
         myForm.email.focus();
-        return false;
     }
-    else document.getElementById("email-error").innerHTML = "";
+    else {
+        document.getElementById("email-error").innerHTML = "";
+        validated.email = true;
+    }
 
+    // If the message field is blank display an error, otherwise remove the error and set its validated state to "true"
     if (message == "") {
         document.getElementById("message-error").innerHTML = "Please enter your a message";
         document.getElementById("required-button").style.color = "red";
         document.getElementById("required-button").innerHTML = "<br>* Required Fields";
         myForm.message.focus();
-        return false;
     }
-    else document.getElementById("message-error").innerHTML = "";
-
-    document.getElementById("required-button").style.color = "green";
-    document.getElementById("required-button").innerHTML = "<br>Message Sent";
+    else {
+        document.getElementById("message-error").innerHTML = "";
+        validated.message = true;
+    }
    
-    // validation passed, submit form
-    document.myForm.submit();
-    
-    return true;
+    // Checking the validation state of the object array, if all states are true - submit the form
+    if(validated.name && validated.phone && validated.email && validated.message == true) {
+        document.getElementById("required-button").style.color = "green";
+        document.getElementById("required-button").innerHTML = "<br>Message Sent";
+        document.myForm.submit();
+    }
 }
